8422501dfe4f13baa25f6fcd534845482148247f
Unknown <tommyboys0107@gmail.com>
2018-01-27 16:36:24 +0800

    [Add] Add ColorMap script and in models.

---
+++ Assets/Script/ColorMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorMap : MonoBehaviour {
+    public static Color Player1;
+    public static Color Player1Ally;
+    public static Color Player2;
+    public static Color Player2Ally;
+
+    public Color player1;
+    public Color player1Ally;
+    public Color player2;
+    public Color player2Ally;
+
+    void Awake()
+    {
+        Player1 = player1;
+        Player1Ally = player1Ally;
+        Player2 = player2;
+        Player2Ally = player2Ally;
+    }
+}
